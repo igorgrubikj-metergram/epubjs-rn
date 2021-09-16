72deeb2393550c13ebb2272b202d0e47ee3de6b7
igorgrubikj <igor.grubikj@gmail.com>
2021-09-16 16:42:17 +0200

    ...

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 process.env.POLYFILL = fs.readFileSync(path.resolve(__dirname, "node_modules/@babel/polyfill/dist/polyfill.min.js"), "utf8");
-process.env.EPUBJS = fs.readFileSync(path.resolve(__dirname, "node_modules/epubjs/build/epub.min.js"), "utf8");
+process.env.EPUBJS = fs.readFileSync(path.resolve(__dirname, "node_modules/epubjs/dist/epub.min.js"), "utf8");
 process.env.BRIDGE = fs.readFileSync(path.resolve(__dirname, "contents/bridge.js"), "utf8");
 
 module.exports = {
